87b9a560baf716c75e1c90e3e03dada55c37b802
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-02-27 20:11:18 +0100

    2

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -5,4 +5,28 @@ document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
+var light = false;
+
+function lightswitch()
+{
+    if(light)
+    {
+        document.body.style.background = "black";
+        btn.innerHTML = "Switch light on";
+        light = false;
+    }
+    else
+    {
+        document.body.style.backgroundColor = "yellow";
+        btn.innerHTML = "Switch light off";
+        light = true;
+    }
+}
+
+document.body.style.backgroundColor = "black";
+btn.innerHTML = "Switch light on";
+btn.onclick = lightswitch;
+
+
+
 // schijf hier tussen je code
